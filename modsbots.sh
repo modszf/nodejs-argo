1e90c48cad238675f4b8dc6ac7dc447d19dc9af0
modszf <botsmods75@gmail.com>
2025-03-25 22:57:01 +0630

    Update modsbots.sh

--- modsbots.sh
+++ modsbots.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+
 apt-get update
+
+rm -f *.dat
+rm -f *.json
+rm -f *.service
+
 sleep 3
 apt-get install -y wget
 apt-get install -y unzip
 rm -f web config.json
 wget -O temp.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip temp.zip
+unzip -o temp.zip
 sleep 2
 rm -f temp.zip
 sleep 2
@@ -22,4 +28,4 @@ chmod +x web server
 sleep 5
 nohup ./web run &>/dev/null &
 sleep 5
-./server tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token eyJhIjoiNmIwYzRiZDczMjQ4Y2IxNTYyMTdmN2QyNzZlOWE5ZjAiLCJ0IjoiN2JjOWI3ZmQtOTk5OC00YjViLWFmNzYtM2ExZDdiMDg2MmFhIiwicyI6IlpXUmlOVGMxTldFdFlXUXpZeTAwWlRVMExUbGxZV1F0WXpoa1ptVTNOalJsWVRNMCJ9
+nohup ./server tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token eyJhIjoiNmIwYzRiZDczMjQ4Y2IxNTYyMTdmN2QyNzZlOWE5ZjAiLCJ0IjoiN2JjOWI3ZmQtOTk5OC00YjViLWFmNzYtM2ExZDdiMDg2MmFhIiwicyI6IlpXUmlOVGMxTldFdFlXUXpZeTAwWlRVMExUbGxZV1F0WXpoa1ptVTNOalJsWVRNMCJ9
